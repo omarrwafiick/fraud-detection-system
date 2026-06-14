4e7def7e41e0e64d418435484ce4d57b077381fe
omarrwafiick <wafickmo11@gmail.com>
2026-06-14 18:06:19 +0300

    add header navigations with styles

--- UI/frontend/src/app/layout/components/app-header/app-header.ts
+++ UI/frontend/src/app/layout/components/app-header/app-header.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, input, output, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { LucideAngularModule, UserRound } from 'lucide-angular';
+import { LucideAngularModule, UserRound, LayoutDashboard, LogOut, UserPlus, ShieldUser } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +10,13 @@ import { LucideAngularModule, UserRound } from 'lucide-angular';
   styleUrl: './app-header.css',
 })
 export class AppHeader {
-  appTitle = input<string>('FRAUD_ARMOR');
-  UserRoundedClass = UserRound;
-  userDisplayName = input<string>('Omar Wafick');
+  appTitle = input<string>('FRAUD ARMOR');
+  UserIconClass = UserRound;
+  LogoutIconClass = LogOut;
+  LoginIconClass = ShieldUser;
+  SignupIconClass = UserPlus;
+  DashboardIconClass = LayoutDashboard;
+  userDisplayName = input<string>('UNKNOWN');
   isLoggedIn = input<boolean>(false);
   logoutClick = output<void>();
   protected isMenuOpen = signal<boolean>(false);
